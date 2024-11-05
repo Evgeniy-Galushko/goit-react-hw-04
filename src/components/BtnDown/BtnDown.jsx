5401097945b0816fb2a44a18e8b8c6168b2af4d3
Galushko Evgeniy <yudginchik@gmail.com>
2024-11-05 18:31:21 +0200

    add

--- src/components/BtnDown/BtnDown.jsx
+++ src/components/BtnDown/BtnDown.jsx
@@ -2,10 +2,8 @@ import s from "./BtnDown.module.css";
 
 export default function BtnDown({ children }) {
   return (
-    <>
-      <a href="#down" className={s.btnNavDown}>
-        {children}
-      </a>
-    </>
+    <a href="#down" className={s.btnNavDown}>
+      {children}
+    </a>
   );
 }
--- src/components/BtnUp/BtnUp.jsx
+++ src/components/BtnUp/BtnUp.jsx
@@ -2,10 +2,8 @@ import s from "./BtnUp.module.css";
 
 export default function BtnUp({ children }) {
   return (
-    <>
-      <a href="#up" className={s.btnNavUp}>
-        {children}
-      </a>
-    </>
+    <a href="#up" className={s.btnNavUp}>
+      {children}
+    </a>
   );
 }
